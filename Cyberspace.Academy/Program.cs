0afed6a3e6e3eab82fed433afaaa8bef8fb3344f
simi <simi@DESKTOP-QUCIPK5>
2022-03-31 10:36:53 -0400

    Add project files.

---
+++ Cyberspace.Academy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CLASSWORK
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int x = 9;
+            int y = 5;
+            Add(x, y);
+            Subtract(x, y);
+            Divide(x, y);
+            Multiply(x, y);
+        }
+
+        static void Add(int x, int y)
+        {
+          
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+        }
+        static void Subtract(int x, int y)
+        {
+            Console.WriteLine("{0} - {1} = {2}", x, y, x - y);
+        }
+        static void Divide(int x, int y)
+        {
+            Console.WriteLine("{0} / {1} = {2}", x, y, x / y);
+        }
+        static void Multiply(int x, int y)
+        {
+            Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+        }
+    }
+}
---
+++ PRACTICE/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JimiPractice
+{
+    public class JimiPracticeNew
+    {
+        static void Main()
+        {
+           
+           var numbers = new int[7] {8, 16, 2, 5, 99, 60, 70};
+            numbers[4] = 26;
+            numbers[3] = numbers[6];
+            numbers[2] = numbers[2] + 2;
+
+            bool[] boolArray = new bool[3] { true, false, false };
+            Console.WriteLine(8); Console.WriteLine(16); Console.WriteLine(boolArray);
+        }
+    }
+}
